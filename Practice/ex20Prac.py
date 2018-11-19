a1d933237ad43e62ab3d4595b2f0b8b34150cfa2
mj268 <markknpr685@gmail.com>
2018-11-19 09:05:29 +0000

    up to exercise 25

--- Practice/ex20Prac.py
+++ Practice/ex20Prac.py
@@ -3,7 +3,7 @@
 #import argv module
 from sys import argv
 #unpack argv into variables
-script, input_file = argv
+script_name, input_file = argv
 #define a function to print the whole file
 def print_all(f):
     print(f.read())
@@ -13,30 +13,30 @@ def rewind(f):
     f.seek(0)
 
 #define a function to print a specific line and print the line number and the line
-def print_a_line(line_count, f):
-    print(line_count, f.readline())
+def print_line(line_number, f):
+    print(line_number, f.readline())
+
 
 #open the file
 current_file = open(input_file)
 
 #print what we will do then call the print whole file method
-print("First let's print the whole file:\n")
-
+print("First we will print the whole file...\n")
 print_all(current_file)
 
 #print what we will do then call the rewind method
-print("Now let's rewind, kind of like a tape.")
-
+print("Now lets rewind")
 rewind(current_file)
 
 #print what we will do then print the three lines individually
-print("Let's print three lines:")
+print("Now lets print each line individually")
 
 current_line = 1
-print_a_line(current_line, current_file)
+
+print_line(current_line, current_file)
 
 current_line = current_line + 1
-print_a_line(current_line, current_file)
+print_line(current_line, current_file)
 
 current_line = current_line + 1
-print_a_line(current_line, current_file)
+print_line(current_line, current_file)
---
+++ ex22.py
@@ -0,0 +1,2 @@
+#This exercise covers all the things learned in exercises 1-21 by writing out the concepts
+
